e9470062eb852eb4560d0d06a14650b9a7185990
Yurii <lurakuz08@gmail.com>
2019-06-10 18:22:49 +0000

    redacted

--- js/index.js
+++
@@ -1,4 +0,0 @@
-const clock = document.createElement('div');
-clock.className = 'clock';
-document.querySelector('header').appendChild(clock);
-setInterval( () => clock.innerText = (new Date().toLocaleTimeString()), 1000);
